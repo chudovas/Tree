20b07b47ed268f372c0b35c92725667011eb72f2
Гурьев Василий Александрович <Vaschudo@gmail.com>
2018-05-20 19:13:57 +0300

    Submit 'Tree' project.

---
+++ TestTree/TestTree.fs
@@ -0,0 +1,61 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open FsUnit
+open NUnit.Framework
+open Tree
+
+[<Test>]
+let ``test of tree 1``() =
+    let tr = new Tree<int>()
+    tr.Add(5)
+    tr.Delete(10)
+    tr.GetList() |> should equal [5]
+
+[<Test>]
+let ``test of tree 2``() =
+    let tr = new Tree<int>()
+    tr.Delete(10)
+    tr.GetList() |> should equal []
+
+[<Test>]
+let ``test of tree 3``() =
+    let tr = new Tree<char>()
+    let mutable res = []
+    tr.Add('d')
+    tr.Add('c')
+    tr.Add('a')
+    tr.Add('b')
+    tr.Add('e')
+    tr.Add('f')
+    
+    for c in tr do
+        res <- c::res
+
+    res |> should equal ['f'; 'e'; 'd'; 'c'; 'b'; 'a']
+
+[<Test>]
+let ``test of tree 4``() =
+    let tr = new Tree<char>()
+    let mutable res = []
+    tr.Add('d')
+    tr.Add('c')
+    tr.Add('a')
+    tr.Add('b')
+    tr.Add('e')
+    tr.Add('f')
+    
+    tr.Delete('a')
+    tr.Delete('b')
+    for c in tr do
+        res <- c::res
+    
+    res |> should equal ['f'; 'e'; 'd'; 'c']
+
+[<EntryPoint>]
+let main argv =
+    ``test of tree 1``()
+    ``test of tree 2``()
+    ``test of tree 3``()
+    ``test of tree 4``()
+    0 // return an integer exit code
---
+++ Tree/Tree.fs
@@ -0,0 +1,57 @@
+﻿open System
+open System.Collections.Generic
+
+
+type InTree<'a> = 
+    | Node of 'a * InTree<'a> * InTree<'a> 
+    | Empty
+
+type Tree<'a when 'a : comparison>() =    
+    let mutable head = InTree.Empty
+    
+    let rec GetListOfElements node =
+        match node with
+        | InTree.Empty -> []
+        | InTree.Node(a, l, r) -> (GetListOfElements l)@[a]@(GetListOfElements r)
+
+    let rec AddRec node add =
+        match node with
+        | InTree.Node(a, l, r) when a < add -> InTree.Node(a, l, AddRec r add)
+        | InTree.Node(a, l, r) when a >= add -> InTree.Node(a, AddRec l add, r)
+        | InTree.Empty -> InTree.Node(add, InTree.Empty, InTree.Empty)
+    
+    let rec Overbalance left node =
+        match left with
+        | InTree.Empty -> node
+        | InTree.Node(a, l, r) -> InTree.Node(a, Overbalance l node, r)
+
+    let rec DeleteRec node del =
+        match node with
+        | InTree.Node(a, l, r) when a < del -> InTree.Node(a, l, DeleteRec r del)
+        | InTree.Node(a, l, r) when a > del -> InTree.Node(a, DeleteRec l del, r)
+        | InTree.Node(a, InTree.Empty, InTree.Empty) when a = del -> InTree.Empty
+        | InTree.Node(a, l, InTree.Empty) when a = del -> l
+        | InTree.Node(a, l, r) when a = del -> Overbalance r l
+        | InTree.Empty -> InTree.Empty
+
+    
+    interface IEnumerable<'a> with
+        member this.GetEnumerator() = 
+            ((new List<'a>(this.GetList())).GetEnumerator() :> Collections.IEnumerator)
+        member this.GetEnumerator() =
+            ((new List<'a>(this.GetList())).GetEnumerator() :> IEnumerator<'a>)
+    
+    member x.Add(add : 'a) =
+        head <- AddRec head add
+
+    member x.Delete(del : 'a) =
+        head <- DeleteRec head del
+
+    member x.GetList() =
+        GetListOfElements head
+
+
+[<EntryPoint>]
+let main argv =
+    0 
+   
